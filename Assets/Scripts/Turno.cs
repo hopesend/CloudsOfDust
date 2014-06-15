355cb993e0792af4d8e9d883fcc1048e6cfb684a
jobao <jobao@69164ae0-0a31-470c-a64e-f8f34697d440>
2014-06-15 00:14:38 +0000

    Creado el sistema de Turnos. Ultimo commit, sin la clase Turno

---
+++ Assets/Scripts/Turno.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turno {
+
+    private PersonajeBase personaje;
+
+    public PersonajeBase Personaje
+    {
+        get { return personaje; }
+    }
+    float tiempoActual = 0;
+
+    bool listo = false;
+
+    public void Listo()
+    {
+        listo = true;
+    }
+
+
+    public float TiempoActual
+    {
+        get { return tiempoActual; }
+    }
+
+    public Turno(PersonajeBase _personaje)
+    {
+        personaje = _personaje;
+    }
+
+    public void Update()
+    {
+        if (!listo)
+        {
+            tiempoActual += personaje.rap.Valor * Time.deltaTime; 
+        }
+    }
+
+    public void UsarHabilidad(float tiempoRestar)
+    {
+        if ((tiempoActual - tiempoRestar) <= 0)
+        {
+            tiempoActual = 0;
+        }
+        else
+        {
+            tiempoActual -= tiempoRestar;
+        }
+
+        listo = false;
+    }
+}
