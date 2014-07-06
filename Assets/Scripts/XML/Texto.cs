a5d492523fe5bc6ff8b427ce6dbfebd14ac26af8
hopesend <hopesend@69164ae0-0a31-470c-a64e-f8f34697d440>
2014-07-06 09:32:52 +0000

    Actualizados: -------------
    - Texto.cs, acabada la clase.
    - Informacion.cs, acabada la clase

--- Assets/Scripts/XML/Texto.cs
+++ Assets/Scripts/XML/Texto.cs
@@ -5,6 +5,9 @@
 /// </summary>
 public class Texto
 {
+	/// <summary>
+	/// Variable que albergara el id buscado
+	/// </summary>
 	private string id;
 	public string Id
 	{
@@ -12,6 +15,9 @@ public class Texto
 		set { id = value; }
 	}
 
+	/// <summary>
+	/// Variable que albergara el nombre del receptor del texto
+	/// </summary>
 	private string receptor;
 	public string Receptor
 	{
@@ -19,6 +25,9 @@ public class Texto
 		set { receptor = value; }
 	}
 
+	/// <summary>
+	/// Variable que albergara el codigo del texto que el receptor tiene que responder
+	/// </summary>
 	private string codigoReceptor;
 	public string CodigoReceptor
 	{
@@ -26,6 +35,9 @@ public class Texto
 		set { codigoReceptor = value; }
 	}
 
+	/// <summary>
+	/// Variable que albergara el texto a mostrar
+	/// </summary>
 	private string textoMostrar;
 	public string TextoMostrar
 	{
@@ -33,7 +45,41 @@ public class Texto
 		set { TextoMostrar = value; }
 	}
 
-	public Texto()
+	/// <summary>
+	/// Constructor de la clase para Conversaciones
+	/// </summary>
+	/// <param name="nuevoId">
+	/// el valor del id que se esta buscando
+	/// </param>
+	/// <param name="nuevoReceptor">
+	/// el nombre el nuevo Receptor de la respuesta a la pregunta
+	/// </param>
+	/// <param name="nuevoCodigo">
+	/// el valor de id de la nueva Pregunta que va asociada al nuevoReceptor
+	/// </param>
+	/// <param name="nuevoTexto">
+	/// El texto a mostrar
+	/// </param>
+	public Texto(string nuevoId, string nuevoReceptor, string nuevoCodigo, string nuevoTexto)
 	{
+		Id = nuevoId;
+		Receptor = nuevoReceptor;
+		CodigoReceptor = nuevoCodigo;
+		TextoMostrar = nuevoTexto;
+	}
+
+	/// <summary>
+	/// Constructor sobrecargado de la clase para textos en HAPQ y/o Pantallas de Ayuda
+	/// </summary>
+	/// <param name="personaje">
+	/// el nombre de personaje u objeto que lanza el texto a pantalla o HAPQ
+	/// </param>
+	/// <param name="nuevoTexto">
+	/// el texto a mostrar
+	/// </param>
+	public Texto(string personaje, string nuevoTexto)
+	{
+		Receptor = personaje;
+		TextoMostrar = nuevoTexto;
 	}
 }
