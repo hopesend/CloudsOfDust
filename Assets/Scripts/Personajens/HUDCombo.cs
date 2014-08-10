c02c68841d0b3259fcbe81f48f4f04f9e54ebacf
gsg3d <gsg3d@69164ae0-0a31-470c-a64e-f8f34697d440>
2014-08-10 14:17:19 +0000

    conmit

--- Assets/Scripts/Personajens/HUDCombo.cs
+++ Assets/Scripts/Personajens/HUDCombo.cs
@@ -17,7 +17,8 @@ public class HUDCombo : MonoBehaviour
 	public Texture buttonC;
     */
 
-	public int sizeTexure;
+	public int sizeTexture;
+	public float  sizeTime;
 	public float marge;
 	public bool inCombo;
 	private bool onPressed;
@@ -27,51 +28,53 @@ public class HUDCombo : MonoBehaviour
 
 	public void Start()
 	{
-		if(sizeTexure == 0) sizeTexure = 50;
+		if(sizeTexture == 0) sizeTexture = 50;
 		if(marge == 0) marge = 0.25f;
 		HEIGHT = Screen.height;
 		WIDTH = Screen.width;
+		if(sizeTime == 0) sizeTime = 1000;
 	}
 	public void OnGUI()
 	{
         if(inCombo) //Muestra todos los botones
 		{
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2, HEIGHT/2 - sizeTexure/2, sizeTexure, sizeTexure), imageCombo)) //S
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2, HEIGHT/2 - sizeTexture/2, sizeTexture, sizeTexture), imageCombo)) //S
 			{
 				GetComponent<Combo>().buttonPressed = "5";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2, HEIGHT/2 - sizeTexure/2 + (sizeTexure + marge), sizeTexure, sizeTexure), imageCombo)) //X
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2, HEIGHT/2 - sizeTexture/2 + (sizeTexture + marge), sizeTexture, sizeTexture), imageCombo)) //X
 			{
 				GetComponent<Combo>().buttonPressed = "2";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2, HEIGHT/2 - sizeTexure/2 - (sizeTexure + marge), sizeTexure, sizeTexure), imageCombo)) //W
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2, HEIGHT/2 - sizeTexture/2 - (sizeTexture + marge), sizeTexture, sizeTexture), imageCombo)) //W
 			{
 				GetComponent<Combo>().buttonPressed = "8";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2 - (sizeTexure + marge), HEIGHT/2 - sizeTexure/2, sizeTexure, sizeTexure), imageCombo)) //A
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2 - (sizeTexture + marge), HEIGHT/2 - sizeTexture/2, sizeTexture, sizeTexture), imageCombo)) //A
 			{
 				GetComponent<Combo>().buttonPressed = "4";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2 + (sizeTexure + marge), HEIGHT/2 - sizeTexure/2, sizeTexure, sizeTexure), imageCombo))//D
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2 + (sizeTexture + marge), HEIGHT/2 - sizeTexture/2, sizeTexture, sizeTexture), imageCombo))//D
 			{
 				GetComponent<Combo>().buttonPressed = "6";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2 + (sizeTexure + marge), HEIGHT/2 - sizeTexure/2 + (sizeTexure + marge), sizeTexure, sizeTexure), imageCombo))//C
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2 + (sizeTexture + marge), HEIGHT/2 - sizeTexture/2 + (sizeTexture + marge), sizeTexture, sizeTexture), imageCombo))//C
 			{
 				GetComponent<Combo>().buttonPressed = "3";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2 + (sizeTexure + marge), HEIGHT/2 - sizeTexure/2 - (sizeTexure + marge), sizeTexure, sizeTexure), imageCombo))//D
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2 + (sizeTexture + marge), HEIGHT/2 - sizeTexture/2 - (sizeTexture + marge), sizeTexture, sizeTexture), imageCombo))//D
 			{
 				GetComponent<Combo>().buttonPressed = "9";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2 - (sizeTexure + marge), HEIGHT/2 - sizeTexure/2 + (sizeTexure + marge), sizeTexure, sizeTexure), imageCombo)) //Z
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2 - (sizeTexture + marge), HEIGHT/2 - sizeTexture/2 + (sizeTexture + marge), sizeTexture, sizeTexture), imageCombo)) //Z
 			{
 				GetComponent<Combo>().buttonPressed = "1";
 			}
-			if(GUI.Button(new Rect(WIDTH/2 - sizeTexure/2 - (sizeTexure + marge), HEIGHT/2 - sizeTexure/2 - (sizeTexure + marge), sizeTexure, sizeTexure), imageCombo)) //Q
+			if(GUI.Button(new Rect(WIDTH/2 - sizeTexture/2 - (sizeTexture + marge), HEIGHT/2 - sizeTexture/2 - (sizeTexture + marge), sizeTexture, sizeTexture), imageCombo)) //Q
 			{
 				GetComponent<Combo>().buttonPressed = "7";
 			}
+			GUI.Box(new Rect(WIDTH/2 - sizeTime/2, HEIGHT/2 + sizeTexture * 2 + marge, sizeTime, sizeTexture/2), "TIME");
 		}
 	}
 }
