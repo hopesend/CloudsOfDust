0159c589a4c87b0a1996687693555554b42bde94
gsg3d <gsg3d@69164ae0-0a31-470c-a64e-f8f34697d440>
2014-08-10 15:38:18 +0000

    conmit

--- Assets/Scripts/Personajens/Combo.cs
+++ Assets/Scripts/Personajens/Combo.cs
@@ -37,7 +37,7 @@ public class Combo : MonoBehaviour
 		EVA = EVASION;
 		result = new float[COMBO.Length];
 	}
-	public float[] SucessCombo() return result; //funcion que retorna el porcentaje del combo
+	public float[] SucessCombo() { return result}; //funcion que retorna el porcentaje del combo
 	private void KeyPressed() //retorna la tecla presionada
 	{
 		if(Input.GetKey(KeyCode.Q)) keyPressed = "Q";
