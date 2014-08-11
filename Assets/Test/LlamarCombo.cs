9cc905892056d1cd51dace287cae3e853702eb60
gsg3d <gsg3d@69164ae0-0a31-470c-a64e-f8f34697d440>
2014-08-11 16:12:11 +0000

    conmit

--- Assets/Test/LlamarCombo.cs
+++ Assets/Test/LlamarCombo.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class LlamarCombo : MonoBehaviour //Test de como llamar a la clase combo
 {
-	public int SUERTE;
-	public int EVASION;
-	public int PUNTERIA;
+	public int suerte;
+	public int evasion;
+	public int punteria;
+	public float time;
 	public bool active;
-	public string[] COMBO;
+	public string[] combo;
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		if(active) GetComponent<Combo>().StartCombo(SUERTE, EVASION, PUNTERIA, COMBO); //LLama a combo (Test)//Se es llamada asi desde la clase HABILIDAD
+		if(active) GetComponent<Combo>().StartCombo(suerte, evasion, punteria, combo, time); //LLama a combo (Test)//Se es llamada asi desde la clase HABILIDAD
 	}
 }
