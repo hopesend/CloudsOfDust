2ceadf1cd0b2bb0eeb25d821fa20628d64821c55
gsg3d <gsg3d@69164ae0-0a31-470c-a64e-f8f34697d440>
2014-08-21 00:54:43 +0000

    conmit

--- Assets/Scripts/Controladores/HAQPHUD.cs
+++ Assets/Scripts/Controladores/HAQPHUD.cs
@@ -1,97 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.IO;
-using System;
+using System.Collections.Generic;
 
 public class HAQPHUD : MonoBehaviour 
 {
-	public bool HUDstatus;
-	public AudioClip soundSelected;
-	private int state;
+	//Start
+	public List<PersonajeControlable> characters;
+	private PersonajeControlable characterSelected;
+
+	private PersonajeControlable character01; //Principal
+	private PersonajeControlable character02;
+	private PersonajeControlable character03;
+	private PersonajeControlable character04;
+	private PersonajeControlable character05;
+
+
+	public List<Texture> texturesCharacter;
+	public List<Texture> texturesGUI;
+	public List<string> configuration;
+
+	private bool HUDstatus;
+	public int state;
 	private float heigthScreen;
 	private float widthScreen;
-	public Texture background;
-	public Texture box;
-	public Texture backButton;
-
 	private Color auxColorGUI;
 	private float fAlpha;
 	public float timeRetard;
 	private float sizeButtonX;
 	private float sizeButtonY;
-	public float aspectRatio;
 	private float offsetX;
-	private float offsetY;
-    
-	//GUI principal
-	private Vector2 scrollPosition;
-	public Texture character1;
-	private Texture character2;
-	private Texture character3;
-	private Texture character4;
-	private Texture character5;
-	public Texture emptyCharacter;
+	private float offsetY; 
+	private float musicVolume = 100;
+	private float effectsVolume = 100;
+	private int indexResolution = 0;
+	private int indexGraphics = 0;
 
-	//GUI OPTIONS
-	private float musicVolume;
-	private float effectsVolume;
-	private bool read;
+	private GUIStyle styleParameters = new GUIStyle();
 
-
-
-	void Update() //Test
-	{
-		if(Input.GetKeyDown(KeyCode.Tab)) 
-		{
-			HUDstatus = !HUDstatus;
-		}
-	} //Test
-
+	void Update() {if(Input.GetKeyDown(KeyCode.Tab)) HUDstatus = !HUDstatus;} //Test
 	void Start()
 	{
+		//characters = GetComponent<GameMaster>().controladoraMundo.personajesSeleccionados; //?
+		characterSelected = character01;
+
 		heigthScreen = Screen.height;
 		widthScreen = Screen.width;
 		auxColorGUI = GUI.color;
-
-		/*background = Resources.Load ("background") as Texture;
-	    rect = Resources.Load("rect") as Texture;
-		backButton = Resources.Load("backButton") as Texture;*/
-
-		sizeButtonY = heigthScreen / (aspectRatio * 10);
-		sizeButtonX = widthScreen / ((aspectRatio / 2) * 10);
-		state = 0;
+		sizeButtonY = heigthScreen / 10;
+		sizeButtonX = widthScreen / 5;
 		offsetX = 0.7f;
 		offsetY = 0.11f;
-		aspectRatio = 1;
-
-		//
-		character2 = emptyCharacter;
-		character3 = emptyCharacter;
-		character4 = emptyCharacter;
-		character5 = emptyCharacter;
+		state = 0;
+		styleParameters.normal.textColor = Color.black;
+		styleParameters.alignment = TextAnchor.MiddleRight;
 	}
-    void OnGUI()
+    private void OnGUI()
     {
 		if(HUDstatus)
 		{
 			Transaction();
 			GUI.Box(new Rect(0, 0, widthScreen, heigthScreen),"");
-			GUI.DrawTexture(new Rect(0, 0, widthScreen, heigthScreen), background);
+			GUI.DrawTexture(new Rect(0, 0, widthScreen, heigthScreen), texturesGUI[0]);
 			//GUI BOX
-			GUI.DrawTexture(new Rect(widthScreen * 0.05f, heigthScreen * 0.11f, widthScreen * 0.6f, heigthScreen * 0.76f), box);
+			GUI.DrawTexture(new Rect(widthScreen * 0.05f, heigthScreen * 0.11f, widthScreen * 0.6f, heigthScreen * 0.76f), texturesGUI[1]);
 			switch(state)
 			{
 			case 0:
 				ButtonHome();
 				break;
 			case 1:
-				ButtonMensage();
+				ButtonMessage();
 				break;
 			case 2:
 				ButtonInventary();
 				break;
 			case 3:
-				ButtonEquipment();
+				ButtonTeam();
 				break;
 			case 4:
 				ButtonDevelopment();
@@ -114,139 +98,200 @@ public class HAQPHUD : MonoBehaviour
 	}
 	void ButtonHome()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * offsetY, sizeButtonX, sizeButtonY), "MENSAJES"))
-		{
-			state = 1;
-		}
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 2), sizeButtonX, sizeButtonY), "INVENTARIO"))
-		{
-			state = 2;
-		}
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 3), sizeButtonX, sizeButtonY), "EQUIPO"))
-		{
-			state = 3;
-		}
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 4), sizeButtonX, sizeButtonY), "DESARROLLO"))
-		{
-			state = 4;
-		}
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 5), sizeButtonX, sizeButtonY), "ESTRATEGIA"))
-		{
-			state = 5;
-		}
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 6), sizeButtonX, sizeButtonY), "BASE DE DATOS"))
-		{
-			state = 6;
-		}
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), "OPCIONES"))
-		{
-			state = 7;
-		}
-		//CHARACTERS
-
-
-		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * 0.14f, heigthScreen * 0.12f, heigthScreen * 0.12f), character1))
-		{
-
-		}
-		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 2), heigthScreen * 0.12f, heigthScreen * 0.12f), character2))
-		{
-			
-		}
-		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 3), heigthScreen * 0.12f, heigthScreen * 0.12f), character3))
-		{
-			
-		}
-		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 4), heigthScreen * 0.12f, heigthScreen * 0.12f), character4))
-		{
-			
-		}
-		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 5), heigthScreen * 0.12f, heigthScreen * 0.12f), character5))
-		{
-			
-		}
-	    /*GUI.BeginGroup(new Rect(widthScreen * 0.08f, heigthScreen * 0.12f, widthScreen * 0.3f, heigthScreen * 0.71f));
-		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(widthScreen * 0.22f), GUILayout.Height(heigthScreen * 0.68f));
-		if(GUILayout.Button("Trasher",  GUILayout.Width(120), GUILayout.Height(120)))
-		{
-
-		}
-		if(GUILayout.Button("Vacio",  GUILayout.Width(120), GUILayout.Height(120)))
-		{
-			
-		}
-		if(GUILayout.Button("Vacio",  GUILayout.Width(120), GUILayout.Height(120)))
-		{
-			
-		}
-		if(GUILayout.Button("Vacio",  GUILayout.Width(120), GUILayout.Height(120)))
-		{
-			
-		}
-		if(GUILayout.Button("Vacio",  GUILayout.Width(120), GUILayout.Height(120)))
-		{
-			
-		}
-		GUILayout.EndScrollView();
-		GUI.EndGroup();*/
-
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * offsetY, sizeButtonX, sizeButtonY), "MENSAJES")) state = state = 1;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 2), sizeButtonX, sizeButtonY), "INVENTARIO")) state = 2;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 3), sizeButtonX, sizeButtonY), "EQUIPO")) state = 3;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 4), sizeButtonX, sizeButtonY), "DESARROLLO")) state = 4;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 5), sizeButtonX, sizeButtonY), "ESTRATEGIA")) state = 5;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 6), sizeButtonX, sizeButtonY), "BASE DE DATOS")) state = 6;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), "OPCIONES")) state = 7;
 	}
-	void ButtonMensage()
+	void ButtonMessage()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		if(GUI.Button(new Rect(widthScreen * 0.1f, heigthScreen * 0.15f,widthScreen * 0.2f, heigthScreen * 0.07f), "NUEVOS"))
+		{
+
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.1f, heigthScreen * 0.25f, widthScreen * 0.2f, heigthScreen * 0.07f), "VISTOS"))
+		{
+			
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.1f, heigthScreen * 0.35f, widthScreen * 0.2f, heigthScreen * 0.07f), "FAVORITOS"))
+		{
+			
+		}
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0;
 	}
 	void ButtonInventary()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0;
 	}
-	void ButtonEquipment()
+	void ButtonTeam()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		if(characters.Count == 1) //Lectura de personajes
+		{
+			character01 = characters [0];
+		}
+		else //textura vacia si no hay personaje
+		{
+			texturesCharacter[1] = texturesCharacter[0];
+		}
+		if(characters.Count == 2)
+		{
+			character02 = characters [1];
+		}
+		else
+		{
+			texturesCharacter[2] = texturesCharacter[0];
+		}
+		if(characters.Count == 3)
+		{
+			character03 = characters [2];
+		}
+		else
+		{
+			texturesCharacter[3] = texturesCharacter[0];
+		}
+		if(characters.Count == 4)
+		{
+			character04 = characters [3];
+		}
+		else
+		{
+			texturesCharacter[4] = texturesCharacter[0];
+		}
+		if(characters.Count == 5)
+		{
+			character05 = characters [5];
+		}
+		else
+		{
+			texturesCharacter[5] = texturesCharacter[0];
+		}
+		//////////////////////////////////////////////////
+		//CHARACTERS
+		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * 0.14f, heigthScreen * 0.12f, heigthScreen * 0.12f), texturesCharacter[0]))
+		{
+			characterSelected = character01;
+			if(characterSelected == null)
+			{
+				Debug.Log("No hay personaje");
+			}
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 2), heigthScreen * 0.12f, heigthScreen * 0.12f), texturesCharacter[1]))
+		{
+			characterSelected = character02;
+			if(characterSelected == null)
+			{
+				Debug.Log("No hay personaje");
+			}
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 3), heigthScreen * 0.12f, heigthScreen * 0.12f), texturesCharacter[2]))
+		{
+			characterSelected = character03;
+			if(characterSelected == null)
+			{
+				Debug.Log("No hay personaje");
+			}
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 4), heigthScreen * 0.12f, heigthScreen * 0.12f), texturesCharacter[3]))
+		{
+			characterSelected = character04;
+			if(characterSelected == null)
+			{
+				Debug.Log("No hay personaje");
+			}
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.065f, heigthScreen * (0.14f * 5), heigthScreen * 0.12f, heigthScreen * 0.12f), texturesCharacter[4]))
+		{
+			characterSelected = character05;
+			if(characterSelected == null)
+			{
+				Debug.Log("No hay personaje");
+			}
+		}
+
+		if(characterSelected == null) //En realidad es != //Test
+		{
+			GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.17f, widthScreen * 0.2f, heigthScreen * 0.12f), "*"/*characterSelected.name*/, styleParameters);
+
+		    GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.45f, widthScreen * 0.2f, heigthScreen * 0.12f), "VITALIDAD: "/*+ characterSelected.Vitalidad*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.50f, widthScreen * 0.2f, heigthScreen * 0.12f), "ESTAMINA: "/*+characterSelected.Estamina*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.55f, widthScreen * 0.2f, heigthScreen * 0.12f), "PM: "/*+characterSelected.PM*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.60f, widthScreen * 0.2f, heigthScreen * 0.12f), "FUERZA: "/*+characterSelected.Fuerza*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.65f, widthScreen * 0.2f, heigthScreen * 0.12f), "RESISTENCIA: "/*+characterSelected.Resistencia*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.16f, heigthScreen * 0.70f, widthScreen * 0.2f, heigthScreen * 0.12f), "CONCENTRACION: "/*+characterSelected.Concentracion*/, styleParameters);
+
+			GUI.Label (new Rect (widthScreen * 0.35f, heigthScreen * 0.45f, widthScreen * 0.2f, heigthScreen * 0.12f), "ESPIRITU: "/*+characterSelected.Espiritu*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.35f, heigthScreen * 0.50f, widthScreen * 0.2f, heigthScreen * 0.12f), "EVASION: "/*+characterSelected.Evasion*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.35f, heigthScreen * 0.55f, widthScreen * 0.2f, heigthScreen * 0.12f), "PUNTERIA: "/*+characterSelected.Punteria*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.35f, heigthScreen * 0.60f, widthScreen * 0.2f, heigthScreen * 0.12f), "RAPIDEZ: "/*+characterSelected.Rapidez*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.35f, heigthScreen * 0.65f, widthScreen * 0.2f, heigthScreen * 0.12f), "SUERTE: "/*+characterSelected.Suerte*/, styleParameters);
+			GUI.Label (new Rect (widthScreen * 0.35f, heigthScreen * 0.70f, widthScreen * 0.2f, heigthScreen * 0.12f), "MOVILIDAD: "/*+characterSelected.Movimiento*/, styleParameters);
+
+			if(GUI.Button(new Rect (widthScreen * 0.43f, heigthScreen * 0.15f, widthScreen * 0.2f, heigthScreen * 0.07f), "MOVIMIENTO"))
+			{
+
+			}
+			if(GUI.Button(new Rect (widthScreen * 0.43f, heigthScreen * 0.25f, widthScreen * 0.2f, heigthScreen * 0.07f), "FISICO"))
+			{
+				
+			}
+			if(GUI.Button(new Rect (widthScreen * 0.43f, heigthScreen * 0.35f, widthScreen * 0.2f, heigthScreen * 0.07f), "MAGICO"))
+			{
+				
+			}
+		}
+		else
+		{
+			GUI.Label (new Rect (widthScreen * 0.3f, heigthScreen * 0.17f, widthScreen * 0.2f, heigthScreen * 0.12f), "Desbloquee jugadores en el mundo¿?", styleParameters);
+		}
+
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0; //Atras
 	}
 	void ButtonDevelopment()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		GUI.Label (new Rect (widthScreen * 0.3f, heigthScreen * 0.17f, widthScreen * 0.2f, heigthScreen * 0.12f), "No hay datos", styleParameters);
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0; //Atras
 	}
 	void ButtonStrategy()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		GUI.Label (new Rect (widthScreen * 0.3f, heigthScreen * 0.17f, widthScreen * 0.2f, heigthScreen * 0.12f), "No hay datos", styleParameters);
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0; //Atras
 	}
 	void ButtonBaseData()
 	{
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		GUI.Label (new Rect (widthScreen * 0.3f, heigthScreen * 0.17f, widthScreen * 0.2f, heigthScreen * 0.12f), "No hay datos", styleParameters);
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0; //Atras
 	}
 	void ButtonOptions()
 	{
-		string path = Application.dataPath;
-		string[] data = new string[5];
-		int count = 0;
-		StreamReader streamReader = new StreamReader (path + "\\Config.ini");
-		StreamWriter streamWriter = new StreamWriter (path + "\\Config.ini");
+		string[] auxResolution = {"800x600", "1200x1024", "1600x900","1920x1080"};
+		string[] auxGraphics = {"Buena", "Muy Buena", "Excelente"};
 
-		string line;
-		using (streamReader)
-		{
-			while ((line = streamReader.ReadLine()) != null)
-			{
-				data[count] = line;
-				count++;
-			}
-		}
 		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 6), sizeButtonX, sizeButtonY), "APLICAR"))
 		{
-
+			configuration[0] = (auxResolution[indexResolution]);
+			configuration[1] = (auxGraphics[indexGraphics]);
+			configuration[2] = musicVolume.ToString();
+			configuration[3] = effectsVolume.ToString();
 		}
-
-
-
-		///////
-
-		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), backButton)) state = 0;
+		if(GUI.Button(new Rect(widthScreen * 0.1f, heigthScreen * 0.3f, widthScreen * 0.25f, heigthScreen * 0.1f), auxResolution[indexResolution]))
+		{
+			if(indexResolution == auxResolution.Length - 1) indexResolution = 0;
+			else indexResolution++;
+		}
+		if(GUI.Button(new Rect(widthScreen * 0.35f, heigthScreen * 0.3f, widthScreen * 0.25f, heigthScreen * 0.1f), auxGraphics[indexGraphics]))
+		{
+			if(indexGraphics == auxGraphics.Length - 1) indexGraphics = 0;
+			else indexGraphics++;
+		}
+		if(GUI.Button(new Rect(widthScreen * offsetX, heigthScreen * (offsetY * 7), sizeButtonX, sizeButtonY), texturesGUI[2])) state = 0; //Atras
 		musicVolume = GUI.HorizontalSlider(new Rect(widthScreen * 0.1f, heigthScreen * 0.5f, widthScreen * 0.5f, 50), musicVolume, 0.0F, 100.0F);
 		effectsVolume = GUI.HorizontalSlider(new Rect(widthScreen * 0.1f, heigthScreen * 0.6f, widthScreen * 0.5f, 50), effectsVolume, 0.0F, 100.0F);
 
 	}
-	void Transaction()
+	private void Transaction()
 	{
 		fAlpha = fAlpha + Time.deltaTime / (timeRetard * 2);
 		//fAlfha texture transaction
